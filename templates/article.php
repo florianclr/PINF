3d0eccc3f895e871c4d05304d94632252ba42ee2
Piroline <75422806+Piroline@users.noreply.github.com>
2021-02-20 18:14:23 +0100

    20/02
    Pop-up ajout ferrure à un devis :
    - Correction bugs affichage des tableaux (prix et options)
    - Ajout du champ pour choisir la quantité d'une ferrure
    - Sélection d'options et leur quantité sur la quantité totale de pièces commandées

--- templates/article.php
+++ templates/article.php
@@ -9,9 +9,12 @@ $produit = valider("produit");
   var tab = ['mediumblue', 'darkred', 'yellowgreen', 'indigo', 'darkcyan'];
 
   var produit="<?php echo $produit; ?>";
+  var img;
   console.log(produit);
 
   var jImg=$('<div class="card h-100" id="imgProduct"><img class="card-img-top" alt=""/></div>');
+
+  var jImgPopUp=$('<div class="card h-100" id="imgProductPopUp"><img class="card-img-top" alt=""/></div>');
 
   var jTitre=$('<div class="card h-100" id="titleProduct"><h4 class="card-title"></h4></div>');
 
@@ -22,34 +25,42 @@ $produit = valider("produit");
   var jLien = $('<a></a>');
 
   var jTable2=$('<div id="T2"><table><tr id="qte"></tr></table></div>');
+  var jTable2PopUp=$('<div id="T2PopUp"><table><tr id="qteP"></tr></table></div>');
 
-  var jLabel=$('<br> <div id="label">Options possibles :</div>');
+  var jLabel=$('<br><div id="label">Options possibles :</div>');
+  var jLabelPopUp=$('<br><div id="labelPopUp">Options possibles :</div>');
 
   var jTable3=$('<div id="T3"><table id="options"></table></div>');
+  var jTable3PopUp=$('<div id="T3PopUp"><table id="optionsP"></table></div>');
+  var jCheckBox=$('<td><input id="checkOpt" type="checkbox"/></td>');
 
   var jButton = $('<input type="button" id="addDevis" value="Ajouter la ferrure à un devis"/>');
   
-  var jPopup = $("<div id='popUpDevis' title='Ajouter la ferrure au devis'>")
-				.append(jImg.clone(true)); 
+  var jPopup = $('<div id="popUpDevis" title="Ajouter la ferrure au devis">');
+  
+  var jQuantite = $('<div id="quantite">Quantité = <input type="number" id="qteFerrure" value="1" min="1"/></div>');
+  
+  var jQuantiteOpt = $('<input type="number" id="qteOpt" value="1" min="1"/>');
   
   var compt = 1;
+  var popup = 0;
 
 	// T1
 	$.ajax({
     url: "libs/dataBdd.php",
     data:{"action":"Produit","idProduit":produit},
-    //data:{"action":"Prix","idProduit":produit},
-    //data:{"action":"Options","idProduit":produit},
     type : "GET",
     success: function(oRep){
       	var couleurFond = tab[(oRep[0].refcategories)-1];
+		img = oRep[0].image;
+
         console.log(oRep);
         console.log(couleurFond);
         
         $(".product").append(jTitre.clone(true).html(oRep[0].titre));
         $("#titleProduct").css("background-color", couleurFond);
         $(".row").append(jImg.clone(true));
-        $(".row .card-img-top").attr('src',"./images/"+oRep[0].image);
+        $(".row .card-img-top").attr('src',"./images/"+img);
         $(".row").append(jDescription.clone(true));
         $("#description").html(oRep[0].description);
         
@@ -65,134 +76,251 @@ $produit = valider("produit");
     },
     dataType: "json"
     });   
-     
-    $.ajax({
-      url: "libs/dataBdd.php",
-      data:{"action":"Dim","idProduit":produit},
-      type : "GET",
-      success: function(oRep){
-        console.log(oRep);
-         $(".contenu").append(jTable2.clone(true)); 
-        if(oRep[0].dimMin!=null && oRep[0].dimMax!=null ){
+  
+  
+  	function tab2_1(popup) {
+		$.ajax({
+		  url: "libs/dataBdd.php",
+		  data:{"action":"Dim","idProduit":produit},
+		  type : "GET",
+		  success: function(oRep){
+		    console.log(oRep);
+		    
+		    // cas insertion dans la page (normal)
+		    if (!popup) { 
+				$(".contenu").append(jTable2.clone(true)); 
+					
+				if(oRep[0].dimMin!=null && oRep[0].dimMax!=null ){
+					$("#qte").append($('<td></td>').html("PU / Quantité"));
+					
+				  	for (var i = 0; i < oRep.length; i++) {
+				    	$("#T2 tbody").append($('<tr></tr>').append($('<td></td>').html(oRep[i].dimMin+" à "+oRep[i].dimMax+" m")).attr("id",i));
+				    	compt++;
+				  	}//fin for 
+						
+				}//fin if
 
-			$("#qte").append($('<td></td>').html("PU / Quantité"));
-			
-          	for (var i = 0; i <oRep.length; i++) {
-            	$("#T2 tbody").append($('<tr></tr>').append($('<td></td>').html(oRep[i].dimMin+" à "+oRep[i].dimMax+" m")).attr("id",i));
-            	compt++;
-          	}//fin for 
+				else {
+					$("#qte").append($('<td></td>').html("Quantité"));
+				  	$("#T2 tbody").append($('<tr id="0"></tr>').append($('<td></td>').html("PU")));
+				  	compt++;
+				}
 				
-        }//fin if
+				console.log("compt="+compt);
+				while (compt > 0) {
+					$(".contenu").append($("</br></br>"));
+					compt--;	
+				}
+			}
+			
+			// -----------------------------------------------------------
+			// cas insertion dans la pop-up
+			else {
+				$("#popUpDevis").append(jTable2PopUp.clone(true)); 
+					
+				if(oRep[0].dimMin!=null && oRep[0].dimMax!=null ){
+					$("#qteP").append($('<td></td>').html("PU / Quantité"));
+					
+				  	for (var i = 0; i < oRep.length; i++) {
+				    	$("#T2PopUp tbody").append($('<tr></tr>').append($('<td></td>').html(oRep[i].dimMin+" à "+oRep[i].dimMax+" m")).attr("id",i+1000));
+				    	compt++;
+				  	}//fin for 
+						
+				}//fin if
 
-        else {
-        	$("#qte").append($('<td></td>').html("Quantité"));
-          	$("#T2 tbody").append($('<tr id="0"></tr>').append($('<td></td>').html("PU")));
-          	compt++;
-        }
-        
-        console.log("compt="+compt);
-        while (compt > 0) {
-			$(".contenu").append($("</br></br>"));
-			compt--;	
-		}
+				else {
+					$("#qteP").append($('<td></td>').html("Quantité"));
+				  	$("#T2PopUp tbody").append($('<tr id="1000"></tr>').append($('<td></td>').html("PU")));
+				  	compt++;
+				}
+				
+				console.log("compt="+compt);
+				while (compt > 0) {
+					$("#popUpDevis").append($("</br></br>"));
+					compt--;	
+				}
+			
+			}
 
-      },
-      error : function(jqXHR, textStatus) {
-        console.log("erreur");
-      },
-      dataType: "json"
-      });  
+		  },
+		  error : function(jqXHR, textStatus) {
+		    console.log("erreur");
+		  },
+		  dataType: "json"
+		  });  
+    }
     
-	// T2
-	$.ajax({
-    url: "libs/dataBdd.php",
-    data:{"action":"Qte","idProduit":produit},
-    type : "GET",
-    success: function(oRep){
-        console.log(oRep);
-        if(oRep.length!=0){
-          $("#description").html(oRep[0].description);
-          
-        
-            for (var i = 0; i<oRep.length; i++) {
-            	if (oRep[i].qteMin == 0)
-            		$("#qte").append($('<td class="qteU"></td>').html("1"));
-            	else
-                	$("#qte").append($('<td class="qteU"></td>').html(" ≥ "+oRep[i].qteMin));
+    function tab2_2(popup) {
+		$.ajax({
+		url: "libs/dataBdd.php",
+		data:{"action":"Qte","idProduit":produit},
+		type : "GET",
+		success: function(oRep){
+		    console.log(oRep);
+		    
+		    // cas insertion dans la page (normal)
+		    if (!popup) {
+				if(oRep.length != 0){
+				
+				    for (var i = 0; i<oRep.length; i++) {
+				    	if (oRep[i].qteMin == 0)
+				    		$("#qte").append($('<td class="qteU"></td>').html("1"));
+				    	else
+				        	$("#qte").append($('<td class="qteU"></td>').html(" ≥ "+oRep[i].qteMin));
 
-                console.log(oRep[i].qteMin);
-                console.log(oRep[i].qteMax);
+				        console.log(oRep[i].qteMin);
+				        console.log(oRep[i].qteMax);
 
-                $.ajax({
-                  url: "libs/dataBdd.php",
-                  data:{"action":"Prix","idProduit":produit,"qteMin":oRep[i].qteMin,"qteMax":oRep[i].qteMax},
-                  type : "GET",
-                  success: function(oRep){
-                      console.log(oRep);
-                      for(var j=0;j<oRep.length;j++){
-                        $('#'+j).append($('<td class="prixUnit"></td>').html(oRep[j].prixU+" €"));
-                      }
-                  },
-                  error : function(jqXHR, textStatus) {
-                    console.log("erreur");
-                  },
-                  dataType: "json"
-                  });
-            
-              
-            }//fin for
-            
-          
-        }//fin if 
-    },
-    error : function(jqXHR, textStatus) {
-      console.log("erreur");
-    },
-    dataType: "json"
-    });//fin 1er requete
-    
+				        $.ajax({
+				          url: "libs/dataBdd.php",
+				          data:{"action":"Prix","idProduit":produit,"qteMin":oRep[i].qteMin,"qteMax":oRep[i].qteMax},
+				          type : "GET",
+				          success: function(oRep){
+				              console.log(oRep);
+				              for(var j=0;j<oRep.length;j++){
+				                $('#'+j).append($('<td class="prixUnit"></td>').html(oRep[j].prixU+" €"));
+				              }
+				          },
+				          error : function(jqXHR, textStatus) {
+				            console.log("erreur");
+				          },
+				          dataType: "json"
+				          });
+				    
+				      
+				    }//fin for
+				    
+				  
+				}//fin if 
+		    }
+		    
+		    // cas insertion dans la pop-up
+		    else {
+		    	if(oRep.length != 0){
+				
+				    for (var i = 0; i<oRep.length; i++) {
+				    	if (oRep[i].qteMin == 0)
+				    		$("#qteP").append($('<td class="qteU"></td>').html("1"));
+				    	else
+				        	$("#qteP").append($('<td class="qteU"></td>').html(" ≥ "+oRep[i].qteMin));
 
-	// T3
-    $.ajax({
-    url: "libs/dataBdd.php",
-    data:{"action":"Options","idProduit":produit},
-    type : "GET",
-    success: function(oRep){
-        console.log(oRep);
-        compt = 0;
-        
-        if(oRep.length!=0){
-          $("#description").html(oRep[0].description);
-          
-          $(".contenu").append(jLabel.clone(true));
-          $(".contenu").append(jTable3.clone(true));
-          for (var i = 0; i< oRep.length; i++) {
-             $("#options").append($('<tr></tr>').attr("id",oRep[i].id));
-             $("#"+oRep[i].id).append($('<td></td>').html(oRep[i].nom)).append($('<td class="prixOpt"></td>').html(oRep[i].prix+" €"));
-             compt++;
-          }
-          
-          console.log("compt="+compt);
-          while (compt > 0) {
-		  	$(".contenu").append($("</br></br>"));
-			compt--;	
-		  }
-        }
+				        $.ajax({
+				          url: "libs/dataBdd.php",
+				          data:{"action":"Prix","idProduit":produit,"qteMin":oRep[i].qteMin,"qteMax":oRep[i].qteMax},
+				          type : "GET",
+				          success: function(oRep){
+				              console.log(oRep);
+				              for(var j=1000;j<(oRep.length+1000);j++){
+				                $('#'+j).append($('<td class="prixUnit"></td>').html(oRep[j-1000].prixU+" €")); // BUG
+				              }
+				          },
+				          error : function(jqXHR, textStatus) {
+				            console.log("erreur");
+				          },
+				          dataType: "json"
+				          });
+				    
+				      
+				    }//fin for
+				    
+				  
+				}//fin if 
+		    }
+		},
+		error : function(jqXHR, textStatus) {
+		  console.log("erreur");
+		},
+		dataType: "json"
+		});//fin 1er requete
+    }
 
-    },
-    error : function(jqXHR, textStatus) {
-      console.log("erreur");
-    },
-    dataType: "json"
-    });
+	function tab3(popup) {
+		$.ajax({
+		url: "libs/dataBdd.php",
+		data:{"action":"Options","idProduit":produit},
+		type : "GET",
+		success: function(oRep){
+		    console.log(oRep);
+		    var qte;
+		    compt = 0;
+		    
+		    // cas insertion dans la page (normal)
+		    if (!popup) {
+				if(oRep.length!=0){
+				  
+				  $(".contenu").append(jLabel.clone(true));
+				  $(".contenu").append(jTable3.clone(true));
+				  
+				  
+				  for (var i = 0; i< oRep.length; i++) {
+				     $("#options").append($('<tr></tr>').attr("id",oRep[i].id));
+				     $("#"+oRep[i].id).append($('<td></td>').html(oRep[i].nom)).append($('<td class="prixOpt"></td>').html(oRep[i].prix+" €"));
+				     compt++;
+				  }
+				  
+				  while (compt > 0) {
+				  	$(".contenu").append($("</br></br>"));
+					compt--;
+				  }
+				}
+		    }
+		    
+		    // cas insertion dans la pop-up
+		    else {
+		    	if(oRep.length != 0){
+				  
+				  $("#popUpDevis").append(jLabelPopUp.clone(true));
+				  $("#popUpDevis").append(jTable3PopUp.clone(true));
+				  
+				  for (var i = 1000; i < (oRep.length+1000); i++) {
+				     $("#optionsP").append($('<tr></tr>').attr("id",oRep[i-1000].id+1000));
+				     $("#"+oRep[i-1000].id+1000).append(jCheckBox.clone(true).attr("id",oRep[i-1000].id+2000))
+				     		 .append($('<td></td>').html(oRep[i-1000].nom))
+				     		 .append($('<td class="prixOpt"></td>').html(oRep[i-1000].prix+" €"));
+				     		 
+				     $("#checkOpt").attr('id', "checkOpt"+oRep[i-1000].id);
+				     		 
+				     $("#checkOpt"+oRep[i-1000].id).click(function() {
+				     	if ($(this).prop("checked") == true) {
+				     		$(this).parent().append(jQuantiteOpt.clone(true));
+				     		qte = $("#qteFerrure").val();
+							console.log(qte);
+							$("#qteOpt").attr('max', qte);
+				     	}
+				     	else if ($(this).prop("checked") == false)
+				     		$("#qteOpt").remove();
+				     });
+				     		 
+				     compt++;
+				  }
+				  
+				  while (compt > 0) {
+				  	$("#popUpDevis").append($("</br></br>"));
+					compt--;
+				  }
+				}
+		    }
+		},
+		error : function(jqXHR, textStatus) {
+		  console.log("erreur");
+		},
+		dataType: "json"
+		});
+	}
+	
+	tab2_1(popup);
+	tab2_2(popup);
+	tab3(popup);
     
 	$(document).ready(function(){
+		var hauteur;
+		
 		$(".contenu").append(jButton.clone(true).click(function() {
 			$(".contenu").append(jPopup.clone(true));
 			$("#popUpDevis").dialog({
 				 modal: true, // permet de rendre le reste de la page inaccesible tant que la pop up est ouverte
-				 height: 600,
-				 width: 600,
+				 height: 800,
+				 width: 1000,
 				 buttons: { // on ajoute des boutons à la pop up 
 				     "Ajouter au devis": function(){
 				     },
@@ -206,12 +334,26 @@ $produit = valider("produit");
 				    $(this).remove(); // supprime la pop up 
 				 }
 			}); 
+			
+			// Réaffichage de l'image de la ferrure
+			$("#popUpDevis").append(jImgPopUp.clone(true));
+			$(".card-img-top").attr('src',"./images/"+img);
+			hauteur = $(".card-img-top").height();
+			$("#imgProductPopUp").css("max-height", hauteur);
+			
+			// Quantité choisie
+			$("#popUpDevis").append(jQuantite.clone(true));
+			
+			
+			// Réaffichage des tableaux des prix + options
+			popup = 1;
+			tab2_1(popup);
+			tab2_2(popup);
+			tab3(popup);
+			
+			
 		}));
 	});
-	
-	function createPopUp(){
-	 
-}
 
 </script>
 <body>
